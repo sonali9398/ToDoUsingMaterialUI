cb0c4d40f0cba0774c9a5b18aa292d06727438ff
Sonali_Karangutkar <60086164+sonali9398@users.noreply.github.com>
2023-08-10 16:54:21 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React  from "react";
+import ToDoList from "./ToDoList";
+// import  ReactDOM from "react-dom";
+import "./App.css"
+
+const App = () =>{
+  return(
+    <>
+      <ToDoList/>, document.getElementById("root");
+    </>
+  )
+}
+
+
+export default App;
